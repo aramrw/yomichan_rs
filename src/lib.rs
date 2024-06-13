0dce2e8707970efb817c3b0819ca0c112bf84f87
aramrw <106574385+aramrw@users.noreply.github.com>
2024-06-13 14:57:54 -0700

    define TermSourceMatchSource

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
 use std::collections::HashMap;
 mod freq;
 
+/// Enum representing what database field was used to match the source term.
+pub enum TermSourceMatchSource {
+    Term,
+    Reading,
+    Sequence,
+}
