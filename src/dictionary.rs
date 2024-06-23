2ae73e21c7f19c38d9073f09ed36bc3bb40f4744
aramrw <106574385+aramrw@users.noreply.github.com>
2024-06-22 18:55:43 -0700

    define NumOrStr

--- src/dictionary.rs
+++ src/dictionary.rs
@@ -7,3 +7,10 @@ use std::collections::HashMap;
 use std::fs;
 use std::io::{self, BufRead, BufReader};
 
+#[allow(dead_code)]
+#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
+pub enum NumOrStr {
+    Num(u64),
+    Str(String),
+}
+
