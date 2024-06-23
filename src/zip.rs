a41f37242a301d28b7347cfe833c846d4e854df0
aramrw <106574385+aramrw@users.noreply.github.com>
2024-06-22 19:05:41 -0700

    Create zip.rs

---
+++ src/zip.rs
@@ -0,0 +1,12 @@
+use crate::dictionary::TermHeadword;
+use crate::errors;
+//use redb::Database;
+use serde::{Deserialize, Serialize};
+use serde_json::Deserializer;
+//use std::collections::HashMap;
+use crate::try_with_line;
+use tempfile::tempdir;
+
+use std::fs;
+use std::io::{self, BufReader, Error as StdIOError, ErrorKind as StdIOErrorKind};
+
