df1f17e83bb39303ea02e68ea3e8b997b2d4ff91
aramrw <106574385+aramrw@users.noreply.github.com>
2024-06-22 19:03:55 -0700

    imports

--- src/lib.rs
+++ src/lib.rs
@@ -8,4 +8,9 @@ mod structured_content;
 mod zip;
 mod tests;
 
+// dictionary.rs
+use crate::dictionary_data::TermEntry;
+use redb::{Database, ReadableTable, TableDefinition};
+use std::error::Error;
+
 }
