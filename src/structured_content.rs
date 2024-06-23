373c78797597441d9414b108494d04b690929556
aramrw <106574385+aramrw@users.noreply.github.com>
2024-06-22 19:05:21 -0700

    define ImageRendering

--- src/structured_content.rs
+++ src/structured_content.rs
@@ -1,2 +1,10 @@
 use serde::{Serialize, Deserialize};
 
+#[allow(dead_code)]
+#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
+pub enum ImageRendering {
+    Auto,
+    Pixelated,
+    CrispEdges,
+}
+
