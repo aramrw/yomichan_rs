19965070678c8ea9d1a0d10479ff3c9c1ceb669c
aramrw <106574385+aramrw@users.noreply.github.com>
2024-06-22 18:59:35 -0700

    define TermEntry

--- src/dictionary_data.rs
+++ src/dictionary_data.rs
@@ -2,3 +2,12 @@ use crate::structured_content::ImageElementBase;
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 
+// dictionary_data.rs
+#[derive(Serialize, Deserialize, Debug)]
+pub struct TermEntry {
+    pub dictionary: String,
+    pub expression: String,
+    pub reading: String,
+    pub sequence: Option<String>,
+}
+
