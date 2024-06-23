a71cefaa84efc386fa26f74d5cd7354b825a8022
aramrw <106574385+aramrw@users.noreply.github.com>
2024-06-22 18:55:32 -0700

    dictionary imports

---
+++ src/dictionary.rs
@@ -0,0 +1,9 @@
+use crate::dictionary_data::TermGlossaryContent;
+use crate::errors;
+use redb::Database;
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+
+use std::fs;
+use std::io::{self, BufRead, BufReader};
+
