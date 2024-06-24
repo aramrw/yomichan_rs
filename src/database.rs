771633ace6bf27c7e684116c62eaa3a1ba94f1e5
aramrw <106574385+aramrw@users.noreply.github.com>
2024-06-23 19:25:22 -0700

    define MediaDataBase

--- src/database.rs
+++ src/database.rs
@@ -11,3 +11,17 @@ use crate::Yomichan;
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 
+// ! Javascript's `ArrayBuffer` most likely fundamentally differs from any Rust ds'
+// ! Should look up how to map these before continuing
+// ! So media can be displayed (or at least stored) within the lib
+// TODO https://github.com/themoeway/yomitan/blob/8eea3661714c64857aa32a8662f7cca6674dd3a4/types/ext/dictionary-database.d.ts#L41
+#[derive(Serialize, Deserialize, Debug)]
+pub struct MediaDataBase<TContentType> {
+    dictionary: String,
+    path: String,
+    media_type: String,
+    width: u16,
+    height: u16,
+    content: TContentType,
+}
+
