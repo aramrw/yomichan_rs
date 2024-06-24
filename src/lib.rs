efb26defc39eceb9cc3ebb9fc0f59f40268741e7
aramrw <106574385+aramrw@users.noreply.github.com>
2024-06-23 19:32:03 -0700

    move add and lookup term to database.rs

--- src/lib.rs
+++ src/lib.rs
@@ -4,15 +4,13 @@ mod dictionary_data;
 mod errors;
 mod freq;
 mod structured_content;
-mod zip;
 mod tests;
+mod zip;
 
 // dictionary.rs
 use crate::dictionary_data::TermEntry;
 use redb::{Database, ReadableTable, TableDefinition};
 use std::error::Error;
-
-const TERMS_TABLE: TableDefinition<&str, &[u8]> = TableDefinition::new("terms");
 
 pub struct Yomichan {
     pub db: Database,
@@ -23,32 +21,5 @@ impl Yomichan {
     pub fn new(db_path: &str) -> Result<Self, errors::InitError> {
         let db = Database::create(format!("{}.redb", db_path))?;
         Ok(Self { db })
-    }
-
-    /// Adds a term entry to the database
-    pub fn add_term(&self, key: &str, term: TermEntry) -> Result<(), Box<dyn Error>> {
-        let tx = self.db.begin_write()?;
-        {
-            let mut table = tx.open_table(TERMS_TABLE)?;
-
-            let term_bytes = bincode::serialize(&term)?;
-            table.insert(key, &*term_bytes)?;
-        }
-        tx.commit()?;
-
-        Ok(())
-    }
-
-    /// Looks up a term in the database
-    pub fn lookup_term(&self, key: &str) -> Result<Option<TermEntry>, Box<dyn Error>> {
-        let tx = self.db.begin_read()?;
-        let table = tx.open_table(TERMS_TABLE)?;
-
-        if let Some(value_guard) = table.get(key)? {
-            let stored_term: TermEntry = bincode::deserialize(value_guard.value())?;
-            Ok(Some(stored_term))
-        } else {
-            Ok(None)
-        }
     }
 }
