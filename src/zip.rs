59eb9bc0f2f899454f669f758f0e4211087fd384
aramrw <106574385+aramrw@users.noreply.github.com>
2024-06-26 21:15:31 -0700

    remove unused imports

--- src/zip.rs
+++ src/zip.rs
@@ -1,14 +1,10 @@
-use crate::dictionary::TermHeadword;
 use crate::errors;
-//use redb::Database;
 use serde::{Deserialize, Serialize};
 use serde_json::Deserializer;
-//use std::collections::HashMap;
-use crate::try_with_line;
 use tempfile::tempdir;
 
 use std::fs;
-use std::io::{self, BufReader, Error as StdIOError, ErrorKind as StdIOErrorKind};
+use std::io::{self, BufReader};
 
 pub type Entries = Vec<Vec<EntryItem>>;
 
