9ac5c85cb3a09228759854d8531e05f394edb9e0
aramrw <106574385+aramrw@users.noreply.github.com>
2024-06-27 21:32:02 -0700

    define DictionaryMessageActionMatchType

--- src/dictionary_worker_handler.rs
+++ src/dictionary_worker_handler.rs
@@ -1,3 +1,12 @@
 use crate::dictionary_importer::ImportDetails;
 use serde::{Deserialize, Serialize};
 
+#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
+pub enum DictionaryMessageActionMatchType {
+    ImportDictionary,
+    DeleteDictionary,
+    GetDictionaryCounts,
+
+}
+
+
