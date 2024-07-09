7c7484da08f2a343914368d8818961d89b176414
aramrw <106574385+aramrw@users.noreply.github.com>
2024-07-09 15:51:02 -0700

    define dict test

--- src/tests/importer_tests.rs
+++ src/tests/importer_tests.rs
@@ -2,3 +2,9 @@ use crate::dictionary_importer::*;
 #[allow(unused_imports)]
 use crate::structured_content::ContentMatchType;
 
+#[test]
+fn dict() {
+    let path = std::path::Path::new("./大辞林第四版");
+    prepare_dictionary(path).unwrap();
+}
+
