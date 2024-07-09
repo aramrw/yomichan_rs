b8172bc772e15d9e4083906ad407af2ef8b464d9
aramrw <106574385+aramrw@users.noreply.github.com>
2024-07-09 15:51:11 -0700

    define hardcoded term_bank test

--- src/tests/importer_tests.rs
+++ src/tests/importer_tests.rs
@@ -8,3 +8,120 @@ fn dict() {
     prepare_dictionary(path).unwrap();
 }
 
+#[test]
+fn hardcoded() {
+    let temp_dir = tempfile::tempdir().unwrap();
+    let dir_path = temp_dir.path().to_str().unwrap();
+
+    assert!(std::path::Path::new(&dir_path).exists());
+
+    let json_1 = r#"
+    [[
+    "糸", 
+    "いと", 
+    "", 
+    "",
+    0, 
+    [
+      {
+        "type": "structured-content",
+        "content": {
+          "tag": "a",
+          "href": "?query=糸を引く&wildcards=off",
+          "content": "糸を引く"
+        }
+      }
+    ],
+    500, 
+    ""
+    ]]
+  "#;
+
+    //looks janky but is valid
+    let json_0 = r#"
+          [
+          [
+            "手信号",
+            "てしんごう",
+            "",
+            "",
+            -1,
+            [
+              {
+                "type": "structured-content",
+                "content": [
+                  {
+                    "content": [
+                      {
+                        "content": [
+                          "〔",
+                          {
+                            "content": [
+                              {
+                                "content": "「てん」",
+                                "data": {
+                                  "name": "語例"
+                                },
+                                "tag": "span"
+                              },
+                              "とも"
+                            ],
+                            "data": {
+                              "name": "補説"
+                            },
+                            "tag": "span"
+                          },
+                          "〕"
+                        ],
+                        "data": {
+                          "name": "補説G"
+                        },
+                        "tag": "div"
+                      }
+                    ],
+                    "data": {
+                      "name": "解説部"
+                    },
+                    "tag": "div"
+                  }
+                ]
+              }
+            ],
+            95500000
+        ]
+        ]
+        "#;
+
+    let json_5 = r#"
+    [[
+    "手数",
+    "てすう",
+    "子",
+    "",
+    -1,
+    [
+      {
+        "type": "structured-content",
+        "content": {
+              "tag": "a",
+              "href": "?query=手数料&wildcards=off",
+              "content": "手数料"
+            }
+      }
+    ],
+    500,
+    ""
+    ]]
+  "#;
+
+    let paths = [
+        format!("{dir_path}\\term_bank_0.json"),
+        format!("{dir_path}\\term_bank_1.json"),
+    ];
+
+    std::fs::write(&paths[0], json_0.as_bytes()).unwrap();
+    //std::fs::write(&paths[1], json_1.as_bytes()).unwrap();
+
+    prepare_dictionary(std::path::Path::new(dir_path)).unwrap();
+}
+
