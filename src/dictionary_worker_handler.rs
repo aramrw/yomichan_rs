f0b82936940b0900f1def647eb3df56acb5e6349
aramrw <106574385+aramrw@users.noreply.github.com>
2024-07-11 22:13:00 -0700

    remove Eq from all dictionary_worker_handler.rs models

--- src/dictionary_worker_handler.rs
+++ src/dictionary_worker_handler.rs
@@ -1,7 +1,7 @@
 use crate::dictionary_importer::ImportDetails;
 use serde::{Deserialize, Serialize};
 
-#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 pub enum DictionaryMessageActionMatchType {
     ImportDictionary,
     DeleteDictionary,
@@ -9,51 +9,51 @@ pub enum DictionaryMessageActionMatchType {
     GetImageDetailsResponse,
 }
 
-#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 pub struct ImportDictionaryMessage {
     /// This is of type [`DictionaryMessageActionMatchType::ImportDictionary`]
     action: DictionaryMessageActionMatchType,
 }
 
-#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 pub struct ImportDictionaryMessageParams {
     details: ImportDetails,
     archive_content: Vec<u8>,
 }
 
-#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 pub struct DeleteDictionaryMessage {
     /// This is of type [`DictionaryMessageActionMatchType::DeleteDictionary`]
     action: DictionaryMessageActionMatchType,
     params: DeleteDictionaryMessageParams,
 }
 
-#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 pub struct DeleteDictionaryMessageParams {
     dictionary_title: String,
 }
 
-#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 pub struct GetDictionaryCountsMessage {
     /// This is of type [`DictionaryMessageActionMatchType::GetDictionaryCounts`]
     action: DictionaryMessageActionMatchType,
     params: GetDictionaryCountsMessageParams,
 }
 
-#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 pub struct GetDictionaryCountsMessageParams {
     dictionary_names: Vec<String>,
     get_total: bool,
 }
 
-#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 pub struct GetImageDetailsResponseMessage {
     /// This is of type [`DictionaryMessageActionMatchType::GetImageDetailsResponse`]
     action: DictionaryMessageActionMatchType,
     params: DictionaryWorkerMediaLoaderHandleMessageParams,
 }
 
-#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 pub struct DictionaryWorkerMediaLoaderHandleMessageParams {
     // Define the fields of this struct based on the actual fields of the JavaScript object
 }
