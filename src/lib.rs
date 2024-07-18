0fe8dd65978d006540dbb008d285e5533a353b8b
aramrw <106574385+aramrw@users.noreply.github.com>
2024-07-17 22:10:55 -0700

    update lib.rs docs + functions

--- src/lib.rs
+++ src/lib.rs
@@ -3,28 +3,40 @@ mod dictionary;
 mod dictionary_data;
 mod dictionary_database;
 mod dictionary_importer;
+mod dictionary_worker_handler;
 mod errors;
 mod freq;
+mod settings;
 mod structured_content;
 mod tests;
-mod dictionary_worker_handler;
 
-// dictionary.rs
-//use crate::dictionary_data::TermEntry;
+use crate::settings::Options;
+
 use redb::{Database as ReDatabase /* ReadableTable, TableDefinition */};
 
-pub struct YCDatabase {
-    pub db: ReDatabase,
-}
+use std::path::Path;
 
 pub struct Yomichan {
-    ycdatabase: YCDatabase,
+    db: ReDatabase,
+    options: Options,
 }
 
-impl YCDatabase {
-    /// Initializes a new Yomichan Dictionary DB Connection
-    pub fn new(db_path: &str) -> Result<Self, errors::InitError> {
-        let db = ReDatabase::create(format!("{}.redb", db_path))?;
-        Ok(Self { db })
+impl Yomichan {
+    /// Initializes _(or if one already exists, opens)_ a Yomichan Dictionary DB Connection.
+    ///
+    /// # Arguments
+    /// 
+    /// * `db_path` - The location where the database will be created.
+    ///
+    /// ```
+    /// use yomichan_rs::Yomichan;
+    /// let ycd = Yomichan::new("C:\\Users\\1\\Desktop");
+    /// ```
+    pub fn new<P: AsRef<Path>>(db_path: P) -> Result<Self, errors::InitError> {
+        let db = ReDatabase::create(db_path)?;
+        Ok(Self {
+            db,
+            options: Options::default(),
+        })
     }
 }
