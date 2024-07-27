e8dc9fccb1031f3f3d90f40fd707ffffc09c9821
aramrw <106574385+aramrw@users.noreply.github.com>
2024-07-26 17:13:39 -0700

    define lookup exact

--- src/database/handlers.rs
+++ src/database/handlers.rs
@@ -5,3 +5,22 @@ use crate::dictionary_data::{
     TermMetaPitch, TermMetaPitchData,
 };
 
+    pub fn lookup_exact<Q: AsRef<str> + Debug>(&self, query: Q) -> Result<VecDBTermEntry, DBError> {
+        let db = DBBuilder::new().open(&DB_MODELS, &self.db_path)?;
+        let rtx = db.r_transaction()?;
+
+        let mut exps = query_sw(&rtx, DatabaseTermEntryKey::expression, query.as_ref())?;
+        let readings = query_sw(&rtx, DatabaseTermEntryKey::reading, query.as_ref())?;
+
+        if exps.is_empty() && readings.is_empty() {
+            return Err(DBError::Query(format!(
+                "no entries found for: {}",
+                query.as_ref()
+            )));
+        }
+
+        exps.extend(readings);
+
+        Ok(exps)
+    }
+
