aff3b84e7d64d5c9c4c127d49e1218fbce578e1d
aramrw <106574385+aramrw@users.noreply.github.com>
2024-08-15 16:49:50 -0700

    create LANGUAGE_DESCRIPTORS

--- src/language/descriptors.rs
+++ src/language/descriptors.rs
@@ -13,3 +13,28 @@ use super::{
     },
 };
 
+static LANGUAGE_DESCRIPTORS: LazyLock<HashMap<&str, LanguageDescriptor<JapanesePreProcessors<'static>, ()>>> = LazyLock::new(|| {
+    HashMap::from([(
+        "ja",
+        LanguageDescriptor {
+            iso: "ja".into(),
+            iso639_3: "jpn".into(),
+            name: "Japanese".into(),
+            example_text: "読め".into(),
+            is_text_lookup_worthy: Some(is_string_partially_japanese),
+            reading_normalizer: None,
+            text_processors: PreAndPostProcessors {
+                pre: JapanesePreProcessors {
+                    convert_half_width_characters: CONVERT_HALF_WIDTH_CHARACTERS,
+                    alphabetic_to_hiragana: ALPHABETIC_TO_HIRAGANA,
+                    normalize_combining_characters: NORMALIZE_COMBINING_CHARACTERS,
+                    alphanumeric_width_variants: ALPHANUMERIC_WIDTH_VARIANTS,
+                    convert_hiragana_to_katakana: CONVERT_HIRAGANA_TO_KATAKANA,
+                    collapse_emphatic_sequences: COLLAPSE_EMPHATIC_SEQUENCES,
+                },
+                post: None,
+            },
+            language_transforms: Some(&JAPANESE_TRANSFORMS),
+        }
+    )])
+});
