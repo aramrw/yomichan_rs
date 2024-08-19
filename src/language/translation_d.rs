f97e4f5c53bcc72ac217e1d6340263164868d937
aramrw <106574385+aramrw@users.noreply.github.com>
2024-08-18 17:08:45 -0700

    define FindKanjiOptions

--- src/language/translation_d.rs
+++ src/language/translation_d.rs
@@ -5,4 +5,14 @@ use regex::Regex;
 use crate::{dictionary::TermSourceMatchType, settings::SearchResolution};
 
 pub type KanjiEnabledDictionaryMap<'a> = HashMap<&'a str, FindKanjiDictionary>;
+
+/// An options object for use with `Translator.findKanji`.
+pub struct FindKanjiOptions<'a> {
+    /// The mapping of dictionaries to search for kanji in.
+    /// The key is the dictionary name.
+    enabled_dictionary_map: KanjiEnabledDictionaryMap<'a>,
+    /// Whether or not non-Japanese characters should be searched.
+    remove_non_japanese_characters: bool,
+}
+
 pub type TermEnabledDictionaryMap<'a> = HashMap<&'a str, FindTermDictionary>;
