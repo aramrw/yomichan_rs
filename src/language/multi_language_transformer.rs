38d118295ee7be936452f8fe9bb57add6c46e841
aramrw <106574385+aramrw@users.noreply.github.com>
2024-10-12 15:11:11 -0700

    Update multi_language_transformer.rs

--- src/language/multi_language_transformer.rs
+++ src/language/multi_language_transformer.rs
@@ -1,5 +1,24 @@
+use crate::language::languages::get_all_language_transform_descriptors;
+
+use super::transformer_d::LanguageTransformer;
 use std::collections::HashMap;
 
-pub struct MultiLanguageTransformer {
-    //language_transformers: HashMap<String, LanguageTransformer>,
+pub struct MultiLanguageTransformer<'a> {
+    language_transformers: HashMap<&'a str, LanguageTransformer>,
+}
+
+impl<'a> MultiLanguageTransformer<'a> {
+    fn new() -> Self {
+        Self {
+            language_transformers: HashMap::new(),
+        }
+    }
+
+    // fn prepare() -> Result<()> {
+    //     let languages_with_transformers = get_all_language_transform_descriptors();
+    //     for lang_and_transforms in languages_with_transformers {
+    //         let language_transformer = LanguageTransformer::new();
+    //         language_transformer.add_descriptor(*lang_and_transforms.language_transforms)
+    //     }
+    // }
 }
