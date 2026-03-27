b55b9de17c7ac44a71f22024ed75ff281feb063a
aramrw <106574385+aramrw@users.noreply.github.com>
2026-03-27 08:36:50 -0700

    fix: rename paltform to platform in EnvironmentInfo

--- src/environment.rs
+++ src/environment.rs
@@ -1,14 +1,14 @@
 use std::{env::consts::OS, sync::LazyLock};
 
 pub struct EnvironmentInfo {
-    paltform: &'static str,
+    pub platform: &'static str,
 }
 
 impl Default for EnvironmentInfo {
     fn default() -> Self {
-        Self { paltform: OS }
+        Self { platform: OS }
     }
 }
 
 pub static CACHED_ENVIRONMENT_INFO: LazyLock<EnvironmentInfo> =
-    LazyLock::new(|| EnvironmentInfo { paltform: OS });
+    LazyLock::new(|| EnvironmentInfo { platform: OS });
